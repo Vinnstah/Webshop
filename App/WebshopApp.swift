afe2561e1694667945c9aa08c897619be3f55a89
Viktor Jansson <v.l.jansson@gmail.com>
2022-09-22 20:16:18 +0200

    Linked spm package to App

--- App/WebshopApp.swift
+++ App/WebshopApp.swift
@@ -6,12 +6,16 @@
 //
 
 import SwiftUI
+import AppFeature
 
 @main
 struct WebshopApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TestView()
+#if os(macOS)
+                .frame(minWidth: 1024, maxWidth: .infinity, minHeight: 512, maxHeight: .infinity)
+#endif
         }
     }
 }
---
+++ Sources/AppFeature/AppFeature.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Viktor Jansson on 2022-09-22.
+//
+
+import Foundation
+import SwiftUI
+
+public struct TestView: View {
+    public init(){}
+    public var body: some View {
+        Text("TEST VIEW")
+    }
+}

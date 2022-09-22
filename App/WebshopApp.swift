15d4ef2039f8fc8cea4067e8a1c0578eb1739891
Viktor Jansson <v.l.jansson@gmail.com>
2022-09-22 20:05:12 +0200

    Add project file

---
+++ App/WebshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebshopApp.swift
+//  Webshop
+//
+//  Created by Viktor Jansson on 2022-09-22.
+//
+
+import SwiftUI
+
+@main
+struct WebshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

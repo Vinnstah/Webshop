598933b9ed2cb8e5bcac61d55f949f666cc525e7
Viktor Jansson <v.l.jansson@gmail.com>
2022-10-11 23:31:37 +0200

    Add SignInFeature tests

--- Tests/SignInFeatureTests/SignInFeatureTests.swift
+++ Tests/SignInFeatureTests/SignInFeatureTests.swift
@@ -3,6 +3,9 @@ import XCTest
 import ComposableArchitecture
 import URLRoutingClient
 import UserModel
+import SiteRouter
+import VaporRouting
+import Vapor
 
 fileprivate extension DependencyValues {
     mutating func setUpDefaults() {
@@ -18,7 +21,7 @@ final class SignInFeatureTests: XCTestCase {
     func testTrivial() throws {
         XCTAssert(true)
     }
-//
+    //
     func test__GIVEN__initial_state__WHEN__emailAddressFieldReceivesInput__THEN__state_email_updates() async throws {
         
         let store = TestStore(
@@ -44,66 +47,120 @@ final class SignInFeatureTests: XCTestCase {
             $0.password = "test123"
         }
     }
-
-
-    func test__GIVEN__initial_state__WHEN__loginButtonPressed__THEN__set_loginInFlight_true_and_set_User_with_email_and_password_and_then_send_loginRequest_to_server() async throws  {
-        //
+    
+    func test__GIVEN__initial_state__WHEN__loginButtonPressed__THEN__set_loginInFlight_true_and_set_User_with_email_and_password_and_then_send_loginRequest_to_server_mock_get_successful_login_back() async throws  {
+        
         let store = TestStore(
             initialState: .init(), //GIVEN initial state
             reducer: SignIn())
-        //
-        store.dependencies.mainQueue = mainQueue.eraseToAnyScheduler()
         
         let user = User(email: "test@test", password: "test123", jwt: "")
+        store.dependencies.mainQueue = mainQueue.eraseToAnyScheduler()
+        let apiClientTest = URLRoutingClient<SiteRoute>
+            .failing
+            .override(.login(user),
+                      with: {
+                try .ok(ResultPayload(forAction: "login", payload: "JWT"))
+            }
+            )
         
-        store.dependencies.urlRoutingClient.override(.login(user), with: {
-            return Result(data: Data(), URLResponse)
-        }
-                                                     )
-        
-        await store.send(.internal(.loginButtonPressed)) {
+        // WHEN loginButtonPressed
+        let task = await store.send(.internal(.loginButtonPressed)) {
             $0.isLoginInFlight = true
             $0.user = User(email: $0.email, password: $0.password, jwt: "")
         }
-        await mainQueue.advance()
         
-       _ = await store.send(.internal(.loginResponse(
+        let request = await store.send(.internal(.loginResponse(
             TaskResult {
-                try await store.dependencies.urlRoutingClient.decodedResponse(for: .login(store.state.user!)).value
+                try await apiClientTest.decodedResponse(
+                    for: .login(User(email: "test@test", password: "test123", jwt: "")),
+                    as:  ResultPayload<String>.self).value.status.get()}))
+        ) {
+            $0.isLoginInFlight = false
+        }
+        
+        let hasUserJwtBeenSetToJWT = ActorIsolated("")
+        store.dependencies.userDefaultsClient.setJWT = { _ , _ in
+            await hasUserJwtBeenSetToJWT.setValue("JWT")
+            return
+        }
+        
+        _ = await store.send(.delegate(.userLoggedIn(jwt: "JWT")))
+        
+        //
+        await hasUserJwtBeenSetToJWT.withValue {
+            XCTAssertEqual($0, "")
+        }
+        await task.cancel()
+        await request.cancel()
+    }
+    
+    func test__GIVEN__initial_state__WHEN__loginButtonPressed__THEN__set_loginInFlight_true_and_set_User_with_email_and_password_and_then_send_loginRequest_to_server_mock_get_failure_back() async throws  {
+        
+        let store = TestStore(
+            initialState: .init(), //GIVEN initial state
+            reducer: SignIn())
+        
+        let user = User(email: "test@test", password: "test123", jwt: "")
+        store.dependencies.mainQueue = mainQueue.eraseToAnyScheduler()
+        let apiClientTest = URLRoutingClient<SiteRoute>
+            .failing
+            .override(.login(user),
+                      with: {
+                try .ok(ResultPayload(forAction: "login", payload: "JWT"))
             }
             )
-            )
-            )
+        
+        // WHEN loginButtonPressed
+        let task = await store.send(.internal(.loginButtonPressed)) {
+            $0.isLoginInFlight = true
+            $0.user = User(email: $0.email, password: $0.password, jwt: "")
+        }
+        
+        let request = await store.send(.internal(.loginResponse(
+            TaskResult {
+                try await apiClientTest.decodedResponse(
+                    for: .login(User(email: "test@test", password: "test123", jwt: "")),
+                    as:  ResultPayload<String>.self).value.status.get()}))
+        ) {
+            $0.isLoginInFlight = false
+        }
+        
+        let hasUserJwtBeenSetToJWT = ActorIsolated("")
+        store.dependencies.userDefaultsClient.setJWT = { _ , _ in
+            await hasUserJwtBeenSetToJWT.setValue("")
+            return
+        }
+        
+        _ = await store.send(.internal(.loginResponse(.failure(ClientError.failedToLogin("Failed to login"))))) {
+            $0.alert = .init(title: TextState("Error"), message: TextState("The operation couldn’t be completed. (UserModel.ClientError error 0.)"), dismissButton: .cancel(TextState("Dismiss"), action: .none))
+        }
+   
+        //
+        await hasUserJwtBeenSetToJWT.withValue {
+            XCTAssertEqual($0, "")
+        }
+        await task.cancel()
+        await request.cancel()
     }
-//
-//        let hasIsLoggedInBeenCalled = ActorIsolated(false)
-//        store.dependencies.userDefaultsClient.boolForKey = { _ in
-//            await hasIsLoggedInBeenCalled.setValue(true)
-//            return false
-//        }
-//
-//        // WHEN onAppear
-//        _ = await store.send(.internal(.onAppear))
-//        await self.mainQueue.advance(by: .seconds(1))
-//        await store.receive(.delegate(.loadIsLoggedInResult(.notLoggedIn)))
-//
-//        // THEN calls isLoggedIn
-//        await hasIsLoggedInBeenCalled.withValue({
-//            XCTAssertTrue($0)
-//        })
-//    }
-//
-//    func test__GIVEN__delegate_action_is_sent__WHEN__delegate_action_is_handled__THEN__do_nothing() async {
-//
-//
-//        let store = TestStore(initialState: .init(), reducer: Splash())
-//        
-//        //GIVEN delegate action is sent
-//        //WHEN received
-//        _ = await store.send(.delegate(.loadIsLoggedInResult(.isLoggedIn)))
-//
-//        // THEN do nothing
-//
-//    }
-//
+    
+    func test__GIVEN__initial_state__WHEN__signUpButtonIsPressed__THEN___do_nothing() async throws {
+        
+        let store = TestStore(
+            initialState: .init(), //GIVEN initial state
+            reducer: SignIn())
+        
+        _ = await store.send(.internal(.signUpButtonPressed))
+        await store.receive(.delegate(.userPressedSignUp))
+    }
+    
+    func test__GIVEN__alert_state__WHEN__signUpButtonIsPressed__THEN___do_nothing() async throws {
+        
+        let store = TestStore(
+            initialState: .init(alert: .init(title: TextState("Error"), message: TextState("The operation couldn’t be completed. (UserModel.ClientError error 0.)"), dismissButton: .cancel(TextState("Dismiss"), action: .none)), //GIVEN initial state
+                                reducer: SignIn()))
+        
+        _ = await store.send(.internal(.signUpButtonPressed))
+        await store.receive(.delegate(.userPressedSignUp))
+    }
 }
